45d36ab9319c08359d23178af41caf95a531db56
orieasy1 <easy1nhard2@gmail.com>
2024-11-25 03:54:02 +0900

    Initial commit

---
+++ src/main/java/com/team3/ossproject/OssprojectApplication.java
@@ -0,0 +1,13 @@
+package com.team3.ossproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssprojectApplication.class, args);
+    }
+
+}
