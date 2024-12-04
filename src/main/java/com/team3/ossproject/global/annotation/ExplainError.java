8a5109efe2004d10bf00fb6d598144be9bbfeb9e
orieasy1 <easy1nhard2@gmail.com>
2024-12-05 03:31:08 +0900

    Feat: global error setting

---
+++ src/main/java/com/team3/ossproject/global/annotation/ExplainError.java
@@ -0,0 +1,13 @@
+package com.team3.ossproject.global.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface ExplainError {
+	String value() default "";
+}
